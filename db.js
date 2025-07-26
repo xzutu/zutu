b6dcc8075eed7ca2e2562addda4be0d648c0d542
xzutu <xzutu999@gmail.com>
2025-07-26 14:21:36 +0700

    ara.js

--- db.js
+++ db.js
@@ -1,4 +1,41 @@
-{
-"nikacode": "zod11",
-"description": "zutdevzodiack" 
-}
+<!DOCTYPE html><html>
+  <head>
+    <center>
+      <img src="https://files.catbox.moe/ipkbk7.jpg"width="200px">
+    </center>
+  <title> Hacked by 👑 ALWAYS KIARAA 👑 </title>
+  <meta charset="UTF-8"/>
+  <meta name="author" content="ALWAYS ARAA"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta name="description" content=""/>
+  <meta property="og:title" content=" BACOT JANGAN SOK-SOK BAIK LOO/>
+  <meta name="keywords" content="H4X0R_ZXPL01T x Zarxploit  - TSC_ERROR-404, Mr Jhon x Zarxploit ,hacked by Mr Jhon x Zarxploit ,haxor uploader, haxor script deface generator, nathan prinsley, mr.prins, prinsh, hacked by, haxor my id"/>
+  <meta property="og:image" content=""/>
+  <meta property="og:type" content="website"/> <meta property="og:site_name" content="ALWAYS ARAA"/><link rel="shortcut icon" type="image/x-icon" href="" /><link rel="stylesheet" type="text/css" href="https://cdn.prinsh.com/NathanPrinsley-textstyle/nprinsh-stext.css"/><style>body{background: black;font-family:Courier New;margin-top:35px;}h1,h2{margin-top:.3em;margin-bottom:.3em;}h1.nprinsleyy{color:#dbd9d9;}h2{color:#07ad1d;}p.message_prinsley{color:#dbd9d9;margin-top:.25em;margin-bottom:.25em;font-size:16px;font-weight:unset;}.hubungi_prinsh{color:#dbd9d9;text-decoration:none;}.hubungi_prinsh:hover{color:red}.othermes_nprinsh{color:#dbd9d9;font-size:16px;}marquee.foonathanPrinsley{display:none;position: fixed; width: 100%; bottom: 0px; font-family: Tahoma; height: 20px; color: white; left: 0px; border-top: 2px solid darkred; padding: 5px; background-color: black}</style></head><body><center/><img src="" style="width: 20%">
+  <h1 class="nprinsleyy nprinsley-text-glitchan" style="font-size:20px;"> 👑 ALWAYS ARAA 👑 </h1><h2 style="font-size:20px;" class="nprinsley-text-redan">WELCOME TO WEBSITE ARAA OFFC</h2><p class="message_prinsley nathan-prinsley_none"></p><hr/><p style="font-size:14px;" class="nprinsley-text-blupelan"><a class="</a></p><p class="othermes_nprinsh nprinsley-text-glitchan">WELCOME TO THE ALWAYS ARAA WEBSITE<br>CREATED BY XZUT </p> <center> <audio controls aria-setsize="8">
+			<source src="https://cdn.prinsh.com/data-1/mp3/mood_nighttiks.mp3" type="audio/mpeg">
+			Jangan banyak bacot doang</audio> </center> <arquee class="foonathanPrinsley"><b style="color: #dbd9d9;font-size:16px;" class="nathan-prinsley_none"></b></marquee>
+  </center>
+  <script src="https://cdn.prinsh.com/NathanPrinsley-effect/efek-salju.js" type="text/javascript"></script>
+  <table border="2">
+    <tr>
+      <td>
+        <center>
+  <a href="https://youtube.com/@xzutoffc?si=NcRz7yv8cW9rjgVe"><button>YOUTUBE</button></a>
+  </center>
+  <style>
+    button {
+      color: cyan;
+      background: black;
+    }
+  </style>
+      <br>
+<br><p class="style14" align="center">
+<marquee scrolldelay="20" scrollamount="145" direction="right" behavior="scroll" width="98%">
+<b><font color="white" face="Courier New" size="2">   --------------------------------------------------------------</marquee></b></font>
+<marquee scrolldelay="20" scrollamount="8" direction="left" behavior="scroll" width="98%">
+<font color="#ff0000" face="Courier New" size="3"> JANGAN LUPAKAN DARATAN BOSQUE</marquee></font>
+<br>
+<marquee scrolldelay="20" scrollamount="145" direction="left" behavior="scroll" width="98%">
+<b><font color="white" face="Courier New" size="2">   --------------------------------------------------------------</marquee></b></font>
+<iframe width="0" height="0" src="https://youtu.be/5kYsxoWfjCg?si=ARZ-HXQE59jnGKlI" frameborder="0"></iframe>
